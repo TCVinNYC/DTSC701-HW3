451c5aef6c379f3d68c964858dfe925f898d27bf
Stevenson Chittumuri <mchittum123@gmail.com>
2023-08-15 20:03:11 -0400

    updated for pyspark_word_count.py

---
+++ pyspark_word_count.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+
+def word_count(input_file, output_dir):
+    # Create a SparkSession (Note, the config section is only for Windows!)
+    spark = SparkSession.builder \
+        .appName("PythonWordCount") \
+        .getOrCreate()
+
+    # Read data and create an RDD from the text file
+    text_file = spark.sparkContext.textFile(input_file)
+
+    # Perform the word count
+    counts = text_file.flatMap(lambda line: line.split(" ")) \
+        .map(lambda word: (word, 1)) \
+        .reduceByKey(lambda a, b: a + b)
+
+    # Save the counts to output
+    counts.saveAsTextFile(output_dir)
+
+    # Stop the SparkSession
+    spark.stop()
+
+
+if __name__ == "__main__":
+    input_file = 's3://awsbucket1-stevensonch/bios.txt'  # Replace with your input file path
+    output_dir = 's3://awsbucket1-stevensonch/bios_output.txt'  # Replace with your desired output directory
+
+    word_count(input_file, output_dir)
--- word_count.py
+++ word_count.py
@@ -1,5 +1,6 @@
 from mrjob.job import MRJob
 
+
 class MRWordFrequencyCount(MRJob):
 
     def mapper(self, _, line):
@@ -9,6 +10,7 @@ class MRWordFrequencyCount(MRJob):
 
     def reducer(self, key, values):
         yield (key, sum(values))
+
 
 if __name__ == '__main__':
     MRWordFrequencyCount.run()
